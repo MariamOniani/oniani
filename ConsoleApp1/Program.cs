0577594d8611ef867b9b3154bb21632fea4f37b8
Mariami Oniani <moniani@credo.ge>
2024-10-23 13:12:31 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name= "mariam";
+string lastname = "oniani";
+int age = 33;
+bool maritalstatus = true;
+
+
+
+
+
+
+
+
+Console.WriteLine($"Hello, World!, my name is {name} {lastname}");
